737e48148db404b8a3fc6cbb94d4dcfebac266a5
Andrey Rodionov <ambal007a@yandex.ru>
2019-03-22 17:31:10 +0400

    Решение задачи 1.2

--- variables/main.js
+++ variables/main.js
@@ -1,5 +1,5 @@
-let x = 2;
-let y = 22;
-let z = 0;
-let result = x * y + 5 * z + x - 1;
-console.log(result);
+let algebra = 4;
+let geography = 5;
+let physics = 4;
+let averageRating = (algebra + geography + physics) / 3;
+console.log (averageRating);
