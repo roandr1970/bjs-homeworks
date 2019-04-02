64867cf931a3a38395d50b83d6dd378b676deb64
Andrey Rodionov <ambal007a@yandex.ru>
2019-04-02 16:36:09 +0400

    Первый коммит по теме Обёртки для примитивов

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,4 @@
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,9 +11,36 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    "use strict";
+   
+    let percent1 = parseInt (percent);
+    let contribution1 = parseInt (contribution);
+    let amount1 = parseInt (amount);
+    
+    if (isNaN(percent1)) {
+        console.log (`Параметр percent содержит неправильное значение ${percent}`); 
+    }
+   
+    if (isNaN(contribution1)) {
+        console.log (`Параметр contribution содержит неправильное значение ${contribution}`); 
+    }
+    
+    if (isNaN(amount1)) {
+        console.log (`Параметр amount содержит неправильное значение ${amount}`); 
+    }
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    let currentDate = new Date();
+    let myDate = new Date (date);
+    let n = (myDate.getFullYear() - currentDate.getFullYear()) * 12;
+    n -= currentDate.getMonth() + 1;
+    n += myDate.getMonth();
+    let amountRefund = amount1 - contribution1;
+    let p = 1/12 * percent1 / 100;
+    let monthlyPayment = amountRefund * (p + p / (Math.pow((1 + p), n) - 1));
+    let totalAmount = contribution1 + monthlyPayment * n;
+    totalAmount = totalAmount.toFixed(2);
+    console.log (totalAmount);
+    return totalAmount;
 }
 
 function sayHello() {
@@ -23,6 +51,11 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    if (Boolean(name) == false) {
+        greeting = "Аноним";
+    } else {
+        greeting = name;
+    }
+    console.log (`Привет, мир! Меня зовут ${greeting}`);
+    return `Привет, мир! Меня зовут ${greeting}`;
 }
