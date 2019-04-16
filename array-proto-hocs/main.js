23e3b7e3bd0ee64e3d3960e2742c7a0b4b3ee33d
Andrey Rodionov <ambal007a@yandex.ru>
2019-04-16 16:19:52 +0400

    ФВП. Передача аргументов и организация поиска

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,5 +1,5 @@
-function compareArrays (arr1,arr2) {
-    (arr1.length === arr2.length) ? console.log ( arr1.every((value, index) => value === arr2[index])) : console.log (false);
+ffunction compareArrays (arr1,arr2) {
+    return (arr1.length === arr2.length) ? arr1.every((value, index) => value === arr2[index]) : false;
 }
 
 compareArrays([1,2,3],[1,2,3]);
@@ -7,11 +7,14 @@ compareArrays([2,3],[1,2,3]);
 compareArrays([3,2,1],[1,2,3]);
 compareArrays([1,2,3],[3]);
 
+
 function memoize(fn, limit) {
-    let result = [{
+    let results = [{
       args: [],
       result
     }];
+    
     return function fn() {
+      const task = results.find(resul => resul.args ===  )
     }
 }
